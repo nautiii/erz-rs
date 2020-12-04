13d5b010aac3b8136715bc87b83a9f49b55d134b
n@uti <quentin.maillard@epitech.eu>
2020-12-04 23:19:52 +0100

    [Del] target folder in libs

--- libs/db/target/rls/debug/deps/db-c288d187cae97a93.d
+++
@@ -1,5 +0,0 @@
-/var/home/quentin/year3/tmp/rust/erz-rs/libs/db/target/rls/debug/deps/db-c288d187cae97a93.rmeta: src/lib.rs
-
-/var/home/quentin/year3/tmp/rust/erz-rs/libs/db/target/rls/debug/deps/db-c288d187cae97a93.d: src/lib.rs
-
-src/lib.rs:
